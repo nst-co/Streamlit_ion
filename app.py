eb0af6d5e10224f6541271b00b8cff5fc2ace776
j_hara <j_hara@nst-co.com>
2023-05-25 17:42:34 +0900

    オーディオファイルの読み込み

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 import librosa as rosa
+import pandas as pd
+import numpy as np
 
-st.title('異音チェッカー シミュレーション')
+st.title("異音チェッカー シミュレーション")
 
+uploaded_files = st.file_uploader(
+    "OKデータのオーディオファイルをアップロードしてください", accept_multiple_files=True
+)
+
+for uploaded_file in uploaded_files:
+    st.write("filename:", uploaded_file.name)
+    wave, sr = rosa.load(uploaded_file, sr=None)
+    times = pd.Index(np.array(range(len(wave))) / sr, name="time(sec)")
+    ts = pd.Series(wave, index=times, name=str(uploaded_file))
+    st.write(ts)
