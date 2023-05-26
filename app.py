0702c6515b7fdcede7ff5548ba3dcdd12572160a
j_hara <j_hara@nst-co.com>
2023-05-26 16:46:34 +0900

    IOで渡すと librosa.load() がm4aを処理できないので、一時ファイルパスの受け渡しに変更 https://discuss.streamlit.io/t/unable-to-load-m4a-files-with-librosa-load-in-streamlit-run/43978

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import librosa as rosa
 import pandas as pd
 import numpy as np
+from tempfile import TemporaryDirectory
+from pathlib import Path
 
 st.title("異音チェッカー シミュレーション")
 
@@ -11,7 +13,10 @@ uploaded_files = st.file_uploader(
 
 for uploaded_file in uploaded_files:
     st.write("filename:", uploaded_file.name)
-    wave, sr = rosa.load(uploaded_file, sr=None)
+    with TemporaryDirectory() as temp_dir:
+        temp_file_path = Path(temp_dir, uploaded_file.name)
+        temp_file_path.write_bytes(uploaded_file.read())
+        wave, sr = rosa.load(temp_file_path, sr=None)
     times = pd.Index(np.array(range(len(wave))) / sr, name="time(sec)")
-    ts = pd.Series(wave, index=times, name=str(uploaded_file))
+    ts = pd.Series(wave, index=times, name=uploaded_file.name)
     st.write(ts)
