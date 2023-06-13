9e6b7e756505e72b8dedc83ae271abda481b9b34
j_hara <j_hara@nst-co.com>
2023-06-13 18:03:44 +0900

    レイアウト変更 タブを止めてサイドバーにする NGの周波数範囲スライダーを無くす（動かすと更新が連続でかかりエラーになるので）

--- app.py
+++ app.py
@@ -59,82 +59,85 @@ def rosa_temp_load_series(file_data) -> Tuple[pd.Series, float]:
     return pd.Series(wave, index=times, name=file_data.name), sr
 
 
+@st.cache_data
+def ok_master(uploaded_ok, d_rate):
+    ss = []
+    master_bar = st.progress(0)
+    for i, file in enumerate(uploaded_ok):
+        ts, sr = rosa_temp_load_series(file)
+        y = dspfir.downsample(ts.values, d_rate)
+        s = waveSpec(y, sr=(sr / d_rate), title=ts.name)
+        ss.append(s)
+        master_bar.progress((i + 1) * 100 // len(uploaded_ok))
+    smax = pd.concat(ss, axis=1).max(axis=1)
+    smax.name = "OKマスター"
+    master_bar.empty()
+    return smax, sr
+
+
+@st.cache_data
+def ng_fft(file, d_rate):
+    ts, sr = rosa_temp_load_series(file)
+    y = dspfir.downsample(ts.values, d_rate)
+    return waveSpec(y, sr=(sr / d_rate), title=ts.name)
+
+
 decimation_rates = [1, 2, 4, 8]
 if "decimation_rate" not in st.session_state:
     st.session_state.decimation_rate = 1
 
 st.title("異音チェッカー シミュレーション")
+placeholder = st.empty()
 
-tabOK, tabNG = st.tabs(["🆗 OK", "🆖 NG"])
-
-with tabOK:
+with st.sidebar:
     uploaded_ok = st.file_uploader(
         "OKデータのオーディオファイルをアップロードしてください（複数可）",
         type=["wav", "mp3", "m4a", "aac", "mp4"],
         accept_multiple_files=True,
     )
+if len(uploaded_ok) == 0:
+    placeholder.warning("←サイドバーからOKデータをアップロードしてください")
+    st.stop()
 
-    @st.cache_data
-    def ok_master(uploaded_ok, d_rate):
-        ss = []
-        master_bar = st.progress(0)
-        for i, uploaded_file in enumerate(uploaded_ok):
-            ts, sr = rosa_temp_load_series(uploaded_file)
-            y = dspfir.downsample(ts.values, d_rate)
-            s = waveSpec(y, sr=sr / d_rate, title=ts.name)
-            ss.append(s)
-            master_bar.progress((i + 1) * 100 // len(uploaded_ok))
-        smax = pd.concat(ss, axis=1).max(axis=1)
-        smax.name = "OKマスター"
-        return smax, sr
+placeholder.empty()
 
-    if len(uploaded_ok) > 0:
-        with st.spinner("OKマスター作成中"):
-            ok, sr = ok_master(uploaded_ok, st.session_state.decimation_rate)
-            sns.relplot(data=ok, aspect=3, kind="line").set(
-                title="OKマスター（最大値）周波数分布", yscale="log", xlim=(0, ok.index[-1])
-            ).set_xlabels("Hz").set_ylabels("amplitude")
-            plotMod(plt)
-            st.write("OKマスターの作成後、NGタブを選択してNGデータをアップロードしてください")
-            st.radio(
-                "サンプリング周波数(周波数分布の最大値はこの半分)",
-                decimation_rates,
-                key="decimation_rate",
-                horizontal=True,
-                format_func=lambda x: f"{round(sr/x/1000):d}kHz",
-            )
+with st.spinner("OKマスター作成中"):
+    ok, sr = ok_master(uploaded_ok, st.session_state.decimation_rate)
+    sns.relplot(data=ok, aspect=3, kind="line").set(
+        title="OKマスター（最大値）周波数分布", yscale="log", xlim=(0, ok.index[-1])
+    ).set_xlabels("Hz").set_ylabels("amplitude")
+    plotMod(plt)
+with st.sidebar:
+    st.radio(
+        "サンプリング周波数(周波数分布の最大値はこの半分)",
+        decimation_rates,
+        key="decimation_rate",
+        horizontal=True,
+        format_func=lambda x: f"{round(sr/x/1000):d}kHz",
+    )
+    uploaded_ng = st.file_uploader(
+        "NGデータのオーディオファイルをアップロードしてください（一つのみ）",
+        type=["wav", "mp3", "m4a", "aac", "mp4"],
+    )
+if uploaded_ng is None:
+    placeholder.warning("←サイドバーからNGデータをアップロードしてください")
+    st.stop()
 
-with tabNG:
-    if len(uploaded_ok) == 0:
-        st.warning("↑OKタブを選択してOKデータをアップロードしてください", icon="⚠️")
-    else:
-        uploaded_ng = st.file_uploader(
-            "NGデータのオーディオファイルをアップロードしてください（一つのみ）",
-            type=["wav", "mp3", "m4a", "aac", "mp4"],
-        )
-        if uploaded_ng is None:
-            st.stop()
+placeholder.empty()
 
-        @st.cache_data
-        def ng_fft(file, d_rate):
-            ng_ts, ng_sr = rosa_temp_load_series(file)
-            y = dspfir.downsample(ng_ts.values, d_rate)
-            return waveSpec(y, sr=(ng_sr / d_rate), title=ng_ts.name)
+ng = ng_fft(uploaded_ng, st.session_state.decimation_rate)
 
-        ng = ng_fft(uploaded_ng, st.session_state.decimation_rate)
+ngxlim = (0.0, ng.index[-1])
 
-        ngmax = float(ng.index[-1])
-        ngxlim = st.slider("表示周波数範囲", 0.0, ngmax, (0.0, ngmax), 1000.0)
-
-        df = pd.DataFrame({"マスター": ok, "NG": ng})
-        sns.relplot(data=df, aspect=3, kind="line", dashes=False, alpha=0.75).set(
-            title=str(ng.name) + "周波数分布比較", yscale="log", xlim=ngxlim
-        ).set_xlabels("Hz").set_ylabels("amplitude")
-        plotMod(plt)
-        ngdb = pd.Series(20 * np.log10(ng / ok), name=ng.name)
-        okdb = ok.apply(lambda x: 0)
-        dfdb = pd.DataFrame({"マスター": okdb, "NG": ngdb})
-        sns.relplot(data=dfdb, aspect=3, kind="line", dashes=False).set(
-            title=str(ng.name) + " マスター比周波数分布比較", ylim=(-15, 15), xlim=ngxlim
-        ).set_xlabels("Hz").set_ylabels("dB")
-        plotMod(plt)
+df = pd.DataFrame({"マスター": ok, "NG": ng})
+sns.relplot(data=df, aspect=3, kind="line", dashes=False, alpha=0.75).set(
+    title=str(ng.name) + "周波数分布比較", yscale="log", xlim=ngxlim
+).set_xlabels("Hz").set_ylabels("amplitude")
+plotMod(plt)
+ngdb = pd.Series(20 * np.log10(ng / ok), name=ng.name)
+okdb = ok.apply(lambda x: 0)
+dfdb = pd.DataFrame({"マスター": okdb, "NG": ngdb})
+sns.relplot(data=dfdb, aspect=3, kind="line", dashes=False).set(
+    title=str(ng.name) + " マスター比周波数分布比較", ylim=(-15, 15), xlim=ngxlim
+).set_xlabels("Hz").set_ylabels("dB")
+plotMod(plt)
